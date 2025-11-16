048faca4fd1127784e66d59b1a3b3c161f630f78
Matheus Paiva <matheuspaiva609@gmail.com>
2025-11-16 03:30:08 -0300

    Finally I could make the floor move properly

--- DinoClone.cpp
+++ DinoClone.cpp
@@ -11,19 +11,23 @@ int main()
     window.setPosition(sf::Vector2i(100, 100));
     window.setFramerateLimit(60);
 
+    //All Sprites that will be used
+
     sf::Texture texture;
     texture.loadFromFile("stylesheet.png");
     if (!texture.loadFromFile("stylesheet.png"))
     {
         std::cout << "Error" << std::endl;
     }
+    //Ground Sprite
 
     sf::Sprite ground(texture), ground_back(texture);
     ground.setTextureRect(sf::IntRect({ 2, 104 }, { 2440, 26 }));
     ground.setPosition(sf::Vector2f(0.f, 520.f));
     ground_back = ground;
 
-    //Sprite e Posição do Dinossaurinho
+    //Dnio sprite and position
+
     constexpr int height = { 94 };
     constexpr int gap = { 24 };
     sf::Sprite dino(texture);
@@ -32,7 +36,9 @@ int main()
         ground.getPosition().y - (height - gap) });
     constexpr int ground_width = 2440;
     float frame_ground = 0.f;
-    //Abre a janela do Windows
+
+    //Open Windows (not the OS lmao)
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -40,14 +46,18 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+        //Ground is moving uooollll
         frame_ground -= 8.f;
         if (frame_ground < -ground_width) {
             frame_ground = 0.f;
         }
+        constexpr int ground_width = { 2440 }; //it's a int about the ground's width
         ground.setPosition(sf::Vector2f(frame_ground, ground.getPosition().y));
+        ground_back.setPosition(sf::Vector2f(frame_ground + (ground_width - 40), ground.getPosition().y)); //This basically makes a second ground that will substitute the other while moving from the screen, this makes the transition more fluid
 
         window.clear(sf::Color::White);
         window.draw(ground);
+        window.draw(ground_back);
         window.draw(dino);
         window.display();
     }
